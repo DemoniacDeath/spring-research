5138da972587b840827950129bb3452bd2cd6192
Michael Skvortsov <michael@mangoerp.eu>
2019-05-18 23:36:46 +0300

    angular tutorial. the outdated shit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Demo';
+  greeting: {id?: string, content?: string} = {};
+  constructor(private http: HttpClient) {
+    http.get("resource").subscribe(data => {
+      this.greeting.id = data['id'];
+      this.greeting.content = data['content'];
+    });
+  }
+}
